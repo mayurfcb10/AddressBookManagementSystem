c40c979fcb71bdbd1cd1c41d5dfc6e213e3fddf2
Mayur <mayurzope555@gmail.com>
2020-09-27 15:36:29 +0530

    [Mayur]ADDED: Created ContactDetails class and AddressMain class with message Welcome to the Address Book Management System

---
+++ ContactDetails.java
@@ -0,0 +1,83 @@
+
+class ContactDetails {
+	private String firstName, lastName, address, city, state, email;
+	private int zip, phoneNumber ;
+
+
+	public ContactDetails(String firstName, String lastName, String address, String city, String state, int zip, int phoneNumber, String email) {
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # AddressBookManagementSystem
+
+ContactDetails class is created taking the contact details as 
+first name, last name, address, city, state, email, phone number, zip
+
+Address Main class created with the Welcome to the Address Book 
+Management System message.
