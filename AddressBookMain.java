bc26394a4b4a00fc40a0e4a644187c0b387c3f47
Mayur <mayurzope555@gmail.com>
2020-09-27 13:22:08 +0530

    [Mayur]ADDED: Welcome to the Address Book Management System

---
+++ AddressBookMain.java
@@ -0,0 +1,8 @@
+/* Welcome to the Address Book Program Code */
+
+public class AddressBookMain {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Address Book Management Program");
+		
+	}
+}		
